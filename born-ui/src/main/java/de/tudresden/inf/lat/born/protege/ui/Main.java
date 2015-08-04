6f654697ee79d7a973337831e9a8cd2c0e14ac7e
Julian Mendez <julian.mendez@tu-dresden.de>
2015-08-04 21:26:50 +0200

    Add class to initialize from Protege

---
+++ born-ui/src/main/java/de/tudresden/inf/lat/born/protege/ui/Main.java
@@ -0,0 +1,42 @@
+package de.tudresden.inf.lat.born.protege.ui;
+
+import java.awt.BorderLayout;
+
+import org.protege.editor.owl.ui.view.cls.AbstractOWLClassViewComponent;
+import org.semanticweb.owlapi.model.OWLClass;
+
+import de.tudresden.inf.lat.born.main.BornStarter;
+
+/**
+ * This is the Protege view component of BORN.
+ * 
+ * @author Julian Mendez
+ */
+public class Main extends AbstractOWLClassViewComponent {
+
+	private static final long serialVersionUID = -5363687740449453246L;
+
+	private BornStarter bornStarter = null;
+
+	@Override
+	public void disposeView() {
+		this.bornStarter.removeListeners();
+	}
+
+	/**
+	 * Initializes the data and GUI. This method is called when the view is
+	 * initialized.
+	 */
+	@Override
+	public void initialiseClassView() {
+		this.bornStarter = new BornStarter(getOWLModelManager().getOWLOntologyManager());
+		this.setLayout(new BorderLayout());
+		add(this.bornStarter.getPanel().getView(), BorderLayout.CENTER);
+	}
+
+	@Override
+	protected OWLClass updateView(OWLClass selectedClass) {
+		return selectedClass;
+	}
+
+}
