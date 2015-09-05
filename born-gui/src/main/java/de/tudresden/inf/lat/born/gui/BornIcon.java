8ad5c0c48ce8389df0687c675d4d6eabdd0e07a6
Julian Mendez <julian.mendez@tu-dresden.de>
2015-09-05 14:58:24 +0200

    Improve class that manages icons

--- born-gui/src/main/java/de/tudresden/inf/lat/born/gui/BornIcon.java
+++ born-gui/src/main/java/de/tudresden/inf/lat/born/gui/BornIcon.java
@@ -3,10 +3,17 @@ package de.tudresden.inf.lat.born.gui;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.net.URL;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 
+/**
+ * This manages the icons.
+ * 
+ * @author Julian Mendez
+ *
+ */
 public class BornIcon {
 
 	public static final String OPEN_FILE_PATH = "icons/openfolder.png";
@@ -19,15 +26,37 @@ public class BornIcon {
 	public static final ImageIcon SAVE_FILE = createIcon(SAVE_FILE_PATH, DEFAULT_ICON_SIZE);
 	public static final ImageIcon RUN = createIcon(RUN_PATH, DEFAULT_ICON_SIZE);
 
-	public static ImageIcon createIcon(String name, int size) {
-		BufferedImage img = null;
+	/**
+	 * Creates an icon with the given size. If the path is invalid, this method
+	 * returns <code>null</code>.
+	 * 
+	 * @param path
+	 *            path of icon
+	 * @param size
+	 *            size of icon
+	 * @return an icon with the given size, or <code>null</code> if the path is
+	 *         invalid
+	 */
+	public static ImageIcon createIcon(String path, int size) {
+		ImageIcon ret = null;
 		try {
-			img = ImageIO.read(BornIcon.class.getClassLoader().getResource(name));
+			URL url = BornIcon.class.getClassLoader().getResource(path);
+			if (url == null) {
+				try {
+					throw new IllegalArgumentException("Icon has an invalid path: '" + path + "'.");
+				} catch (IllegalArgumentException e) {
+					e.printStackTrace();
+				}
+
+			} else {
+				BufferedImage img = ImageIO.read(url);
+				ret = new ImageIcon(img.getScaledInstance(size, size, Image.SCALE_SMOOTH));
+
+			}
 		} catch (IOException e) {
-			throw new RuntimeException(e);
+			e.printStackTrace();
 		}
-		Image dimg = img.getScaledInstance(size, size, Image.SCALE_SMOOTH);
-		return new ImageIcon(dimg);
+		return ret;
 	}
 
 }
