1a430f5778233b746cb4e3d8381a73defda534f8
Julian Mendez <julian.mendez@tu-dresden.de>
2015-10-25 01:59:20 +0200

    Add sorting of examples in example loader

--- born-owlapi/src/main/java/de/tudresden/inf/lat/born/owlapi/example/ExampleLoader.java
+++ born-owlapi/src/main/java/de/tudresden/inf/lat/born/owlapi/example/ExampleLoader.java
@@ -9,6 +9,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.jar.JarEntry;
@@ -132,6 +133,7 @@ public class ExampleLoader {
 		} else {
 			ret.addAll(getExampleFilesFromDirectory(path));
 		}
+		Collections.sort(ret);
 		return ret;
 	}
 
