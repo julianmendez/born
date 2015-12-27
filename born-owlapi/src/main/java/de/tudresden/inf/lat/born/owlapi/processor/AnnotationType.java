b30fb4498f98f0de18dea8d80c7a9480359a4b69
Julian Mendez <julian.mendez@tu-dresden.de>
2015-12-27 15:48:48 +0100

    Extend Javadoc

--- born-owlapi/src/main/java/de/tudresden/inf/lat/born/owlapi/processor/AnnotationType.java
+++ born-owlapi/src/main/java/de/tudresden/inf/lat/born/owlapi/processor/AnnotationType.java
@@ -3,6 +3,12 @@ package de.tudresden.inf.lat.born.owlapi.processor;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * This enumeration models the type of an annotation.
+ * 
+ * @author Julian Mendez
+ *
+ */
 public enum AnnotationType {
 
 	Byte, //
@@ -20,10 +26,20 @@ public enum AnnotationType {
 	UnsignedShort, //
 	UnsignedByte; //
 
+	/**
+	 * Returns the name.
+	 * 
+	 * @return the name
+	 */
 	public String getName() {
 		return name().substring(0, 1).toLowerCase() + name().substring(1);
 	}
 
+	/**
+	 * Returns the values.
+	 * 
+	 * @return the values
+	 */
 	public static List<AnnotationType> getValues() {
 		return Arrays.asList(values());
 	}
