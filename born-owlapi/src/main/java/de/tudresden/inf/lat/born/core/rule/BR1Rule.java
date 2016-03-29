e466f79b4084a213f137488bb0ce17dae9908b76
Julian Mendez <julian.mendez@tu-dresden.de>
2016-03-29 19:03:53 +0200

    Make small correction in Javadoc

--- born-owlapi/src/main/java/de/tudresden/inf/lat/born/core/rule/BR1Rule.java
+++ born-owlapi/src/main/java/de/tudresden/inf/lat/born/core/rule/BR1Rule.java
@@ -10,7 +10,7 @@ import de.tudresden.inf.lat.born.core.term.Term;
  * This is a basic completion rule for ProbLog.
  * <ul>
  * <li><b>if</b> X is concept in <i>T</i><br>
- * <b>then</b> S := S &cup; {X \u2291 B}</li>
+ * <b>then</b> S := S &cup; {X \u2291 X}</li>
  * </ul>
  * <br>
  * 
