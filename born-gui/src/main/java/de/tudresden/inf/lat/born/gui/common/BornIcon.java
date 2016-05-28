ab6560e449d0c2276bf33ee627778bfa2a193b28
Julian Mendez <julian.mendez@tu-dresden.de>
2016-05-28 08:15:31 +0200

    Add method to load images

--- born-gui/src/main/java/de/tudresden/inf/lat/born/gui/common/BornIcon.java
+++ born-gui/src/main/java/de/tudresden/inf/lat/born/gui/common/BornIcon.java
@@ -18,6 +18,8 @@ import javax.swing.ImageIcon;
  */
 public class BornIcon {
 
+	public static final String PATH_BACKGROUND = "images/background.png";
+
 	public static final String PATH_OPEN_FILE = "icons/openfolder.png";
 	public static final String PATH_SAVE_FILE = "icons/floppydisk.png";
 	public static final String PATH_VIEW_FILE = "icons/magnifyingglass.png";
@@ -27,12 +29,46 @@ public class BornIcon {
 
 	public static final int DEFAULT_ICON_SIZE = 24;
 
+	public static final BufferedImage BACKGROUND = createImage(PATH_BACKGROUND).get();
+
 	public static final ImageIcon OPEN_FILE = createIcon(PATH_OPEN_FILE).get();
 	public static final ImageIcon SAVE_FILE = createIcon(PATH_SAVE_FILE).get();
 	public static final ImageIcon VIEW_FILE = createIcon(PATH_VIEW_FILE).get();
 	public static final ImageIcon RUN = createIcon(PATH_RUN).get();
 	public static final ImageIcon REFRESH = createIcon(PATH_REFRESH).get();
 	public static final ImageIcon BACK = createIcon(PATH_BACK).get();
+
+	/**
+	 * Returns an optional containing an image if the path is valid, or an empty
+	 * optional if the path is invalid.
+	 * 
+	 * @param path
+	 *            path of image
+	 * @return an optional containing an image if the path is valid, or an empty
+	 *         optional if the path is invalid
+	 */
+	public static Optional<BufferedImage> createImage(String path) {
+		Objects.requireNonNull(path);
+		Optional<BufferedImage> ret = Optional.empty();
+		try {
+			URL url = BornIcon.class.getClassLoader().getResource(path);
+			if (Objects.isNull(url)) {
+				try {
+					throw new IllegalArgumentException("Image has an invalid path: '" + path + "'.");
+				} catch (IllegalArgumentException e) {
+					e.printStackTrace();
+				}
+
+			} else {
+				BufferedImage image = ImageIO.read(url);
+				ret = Optional.of(image);
+
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return ret;
+	}
 
 	/**
 	 * Returns an optional containing an icon with the given size if the path is
@@ -49,22 +85,10 @@ public class BornIcon {
 		Objects.requireNonNull(path);
 		Optional<ImageIcon> ret = Optional.empty();
 		if (size >= 0) {
-			try {
-				URL url = BornIcon.class.getClassLoader().getResource(path);
-				if (Objects.isNull(url)) {
-					try {
-						throw new IllegalArgumentException("Icon has an invalid path: '" + path + "'.");
-					} catch (IllegalArgumentException e) {
-						e.printStackTrace();
-					}
-
-				} else {
-					BufferedImage img = ImageIO.read(url);
-					ret = Optional.of(new ImageIcon(img.getScaledInstance(size, size, Image.SCALE_SMOOTH)));
-
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
+			Optional<BufferedImage> optImage = createImage(path);
+			if (optImage.isPresent()) {
+				BufferedImage image = optImage.get();
+				ret = Optional.of(new ImageIcon(image.getScaledInstance(size, size, Image.SCALE_SMOOTH)));
 			}
 		}
 		return ret;
