d33f63395b2438e2a31873cc3e50ef42c1c619f0
Julian Mendez <julian.mendez@tu-dresden.de>
2016-06-16 19:13:22 +0200

    Update queries of examples to use prefixes

--- born-owlapi/src/test/java/de/tudresden/inf/lat/born/owlapi/example/ExampleLoaderTest.java
+++ born-owlapi/src/test/java/de/tudresden/inf/lat/born/owlapi/example/ExampleLoaderTest.java
@@ -23,54 +23,42 @@ public class ExampleLoaderTest {
 			ExampleConfiguration conf = list.get(0);
 			Assert.assertEquals("example-0", conf.getOntologyName());
 			Assert.assertTrue(conf.getOntologyFileName().endsWith("/examples/example-0.owl"));
-			Assert.assertEquals(
-					"query(sub('http://lat.inf.tu-dresden.de/systems/born/born-example#a', 'http://lat.inf.tu-dresden.de/systems/born/born-example#e')).\n\n",
-					conf.getQuery());
+			Assert.assertEquals("query(sub('ont:a', 'ont:e')).\n\n", conf.getQuery());
 		}
 
 		{
 			ExampleConfiguration conf = list.get(1);
 			Assert.assertEquals("example-1", conf.getOntologyName());
 			Assert.assertTrue(conf.getOntologyFileName().endsWith("/examples/example-1.owl"));
-			Assert.assertEquals(
-					"query(sub('http://lat.inf.tu-dresden.de/systems/born/born-example#a', 'http://lat.inf.tu-dresden.de/systems/born/born-example#e')).\n\n",
-					conf.getQuery());
+			Assert.assertEquals("query(sub('ont:a', 'ont:e')).\n\n", conf.getQuery());
 		}
 
 		{
 			ExampleConfiguration conf = list.get(2);
 			Assert.assertEquals("example-2", conf.getOntologyName());
 			Assert.assertTrue(conf.getOntologyFileName().endsWith("/examples/example-2.owl"));
-			Assert.assertEquals(
-					"query(inst('http://lat.inf.tu-dresden.de/systems/born/born-example#i0', 'http://lat.inf.tu-dresden.de/systems/born/born-example#e')).\n\n",
-					conf.getQuery());
+			Assert.assertEquals("query(inst('ont:i0', 'ont:e')).\n\n", conf.getQuery());
 		}
 
 		{
 			ExampleConfiguration conf = list.get(3);
 			Assert.assertEquals("example-3", conf.getOntologyName());
 			Assert.assertTrue(conf.getOntologyFileName().endsWith("/examples/example-3.owl"));
-			Assert.assertEquals(
-					"query(inst('http://lat.inf.tu-dresden.de/systems/born/born-example#r', 'http://lat.inf.tu-dresden.de/systems/born/born-example#i0', 'http://lat.inf.tu-dresden.de/systems/born/born-example#i1')).\n\n",
-					conf.getQuery());
+			Assert.assertEquals("query(inst('ont:r', 'ont:i0', 'ont:i1')).\n\n", conf.getQuery());
 		}
 
 		{
 			ExampleConfiguration conf = list.get(4);
 			Assert.assertEquals("example-4", conf.getOntologyName());
 			Assert.assertTrue(conf.getOntologyFileName().endsWith("/examples/example-4.owl"));
-			Assert.assertEquals(
-					"query(inst('http://lat.inf.tu-dresden.de/systems/born/born-example#r', 'http://lat.inf.tu-dresden.de/systems/born/born-example#i0', 'http://lat.inf.tu-dresden.de/systems/born/born-example#i1')).\n\n",
-					conf.getQuery());
+			Assert.assertEquals("query(inst('ont:r', 'ont:i0', 'ont:i1')).\n\n", conf.getQuery());
 		}
 
 		{
 			ExampleConfiguration conf = list.get(5);
 			Assert.assertEquals("example-5", conf.getOntologyName());
 			Assert.assertTrue(conf.getOntologyFileName().endsWith("/examples/example-5.owl"));
-			Assert.assertEquals(
-					"query(sub('http://lat.inf.tu-dresden.de/systems/born/born-example#a', 'http://lat.inf.tu-dresden.de/systems/born/born-example#d')).\n\n",
-					conf.getQuery());
+			Assert.assertEquals("query(sub('ont:a', 'ont:d')).\n\n", conf.getQuery());
 		}
 
 	}
